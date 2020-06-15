e42c3c1957dfe736379ed215b136452c59c59497
Christofer Flores <cristofer.flort@gmail.com>
2020-06-15 17:59:05 -0500

    Finish Sorting list

--- app.js
+++ app.js
@@ -36,6 +36,51 @@ function progressBar() {
   })
 }
 
+/**
+ * Add a listener on add button to get the input value
+ * and append it to the list
+ * Add listner to sort button to get all the list elements
+ * sort them and replace the list with the new items
+ */
+function sortingList() {
+  var addBtn = $('#sorting-list-container #add-item')
+  var sortBtn = $('#sorting-list-container #sort-items')
+  var newItemInput = $('#sorting-list-container #new-item-input')
+  var itemList = $('#sorting-list-container .lists > ul')
+
+  // Handler for new item
+  addBtn.addEventListener('click', function () {
+    var newItem = newItemInput.value
+    var listItem = document.createElement('li') // Create the list item element
+
+    listItem.textContent = newItem // Assign the correct text from the input
+    itemList.appendChild(listItem) // Append it to the list
+
+    newItemInput.value = '' // Clear the input
+  })
+
+  // Handler to sort elements
+
+  sortBtn.addEventListener('click', function () {
+    var listElements = itemList.querySelectorAll('li') // Query all li elements
+    var items = [] // Array to store only the text from the li elements
+
+    for (i = 0; i < listElements.length; ++i) {
+      items[i] = listElements[i].textContent; // Store the textContent
+    }
+
+    itemList.innerHTML = "" // Remove all the elements inside the list
+
+    items.sort().forEach(function (item) { // Sort it and iterate
+      var newListItem = document.createElement('li')
+
+      newListItem.textContent = item
+      itemList.appendChild(newListItem)
+    })
+  })
+}
+
 
 // Call the functions
 progressBar()
+sortingList()
